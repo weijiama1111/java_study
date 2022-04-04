40649682c75e1f37285d7e656fb7b57be4432338
weijiama1111 <weijiama1111@163.com>
2022-04-04 15:40:22 +0800

    first commit

---
+++ src/helloWorld.java
@@ -0,0 +1,8 @@
+public class helloWorld {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("hello World"); // 输出 Hello World
+    }
+}
